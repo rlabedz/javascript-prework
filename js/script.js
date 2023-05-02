f8503b1c1097081a7325f75462b129639b610f46
Rafal Labedz <r.labedz@ravsol.pl>
2023-05-02 12:55:40 +0200

    Add playerMove in script.js

--- js/script.js
+++ js/script.js
@@ -9,10 +9,28 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if(randomNumber == 2){
-    computerMove = 'nozyce';
+    computerMove = 'papier';
   }
 else if (randomNumber == 3){
-    computerMove = 'papier';
+    computerMove = 'nozyce';
   }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+    playerMove = 'papier';
+  }
+else if(playerInput == '3'){
+    playerMove = 'nozyce';
+  }
+
+printMessage('Twój ruch to: ' + playerMove);
