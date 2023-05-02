3de26961d6d9f08b83ece1306c6c08c10032b1c8
Rafal Labedz <r.labedz@ravsol.pl>
2023-05-02 13:09:52 +0200

    Add winner in script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Wywałanie funkcji computerMove
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
@@ -17,6 +18,8 @@ else if (randomNumber == 3){
 
 printMessage('Mój ruch to: ' + computerMove);
 
+// Wywałanie funkcji playerMove
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -34,3 +37,43 @@ else if(playerInput == '3'){
   }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// Wywałanie funkcji wynik
+
+//Kamien
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+else if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+else if( computerMove == 'kamień' && playerMove == 'nozyce'){
+    printMessage('Ja wygrywam!');
+  }
+
+//Papier
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam!');
+  }
+else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+else if( computerMove == 'papier' && playerMove == 'nozyce'){
+    printMessage('Ty wygrywasz!');
+  }
+
+//Nozyce
+  else if( computerMove == 'nozyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+else if( computerMove == 'nozyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!');
+  }
+else if( computerMove == 'nozyce' && playerMove == 'nozyce'){
+    printMessage('Remis!');
+  }
+
+//Nieznany ruch
+else if(playerMove == 'nieznany ruch'){
+    printMessage('Wykonałeś nieznany ruch!');
+  } 
